89fce500957db6c964d55bad87f5943835f0d299
MWBradshaw <mbradshaw@claflin.edu>
2022-07-31 19:30:51 -0400

    This commit will update the Timer component

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,23 +1,70 @@
 import { useEffect, useState } from 'react';
 import styled from 'styled-components'; 
 
+const HeaderText = styled.p`
+    font-family: Share Tech, sans serif;
+    font-weight: 400px;
+    font-size: 40px;
+    color: white;
+    text-align: center;
+    margin: 46 0px;
+`
 
 const TimerText = styled.p`
     font-family: Orbitron, sans-serif;
     font-weight: 900;
+    font-size: 60px;
+    color: white;
+    text-align: center;
+    align-self: center;
+`
+
+const TimerBody = styled.div`
+    height: 345px;
+    display: flex;
+    margin-bottom: 80px;
+    justify-content: center;
+`
+
+const TimerButtonBody = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+`
+
+
+const TimerButton = styled.button`
+    height: 65px;
+    width: 340px;
+    border: 0px;
+    border-radius: 30px;
+    margin-bottom: 30px;
 `
 
 const Timer = () => {
-    const [timer, setTimer] = useState('00:00:00');
+    const [timer, setTimer] = useState('00:25:00');
 
     useEffect(() => {
-        setTimer('00:00:00');
-    }, [])
-
-
-
+        document.body.style.background = "#200910";
+        setTimer('00:25:00');
+    }, []);
 
     return(
-        <TimerText>{timer}</TimerText>
+        <>
+        <HeaderText>
+            My Pomodoro
+        </HeaderText>
+
+        <TimerBody>
+            <TimerText>{timer}</TimerText>
+        </TimerBody>
+
+        <TimerButtonBody>
+            <TimerButton>Start Timer</TimerButton>
+            <TimerButton>Reset Timer</TimerButton>
+            <TimerButton>Customize Timer</TimerButton>
+        </TimerButtonBody>
+        </>
     );
 }
+
+export default Timer;
